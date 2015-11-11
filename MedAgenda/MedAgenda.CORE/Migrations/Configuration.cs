12fe928bbf7b895dd309b679e6543dc0efcf2826
TJ Delaney <TJDelaney0@gmail.com>
2015-11-11 15:01:37 -0800

    EmergencyContactController/Tests

---
+++ MedAgenda/MedAgenda.CORE/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace MedAgenda.CORE.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MedAgenda.CORE.Infrastructure.MedAgendaDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MedAgenda.CORE.Infrastructure.MedAgendaDbContext";
+        }
+
+        protected override void Seed(MedAgenda.CORE.Infrastructure.MedAgendaDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
