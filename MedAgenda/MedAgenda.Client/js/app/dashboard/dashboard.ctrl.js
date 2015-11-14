2cbe5f32ff43f91dee3f17a4bea80d53817af545
jeffward01 <jeffwardcoding@gmail.com>
2015-11-14 13:42:24 -0800

    added tabs to dashboard

--- MedAgenda/MedAgenda.Client/js/app/dashboard/dashboard.ctrl.js
+++ MedAgenda/MedAgenda.Client/js/app/dashboard/dashboard.ctrl.js
@@ -1,6 +1,24 @@
-﻿angular.module('app').controller('DashboardController', function ($rootScope, $scope) {
-    $rootScope.$broadcast('change-page-title', { title: 'Dashboard' });
+angular.module('app').controller('DashboardController', function ($rootScope, $scope) {
+    $rootScope.$broadcast('change-page-title', {
+        title: 'Dashboard'
+    });
+    $scope.tabs = [
+        {
+            title: 'Dynamic Title 1',
+            content: 'Dynamic content 1'
+        },
+        {
+            title: 'Dynamic Title 2',
+            content: 'Dynamic content 2',
+            disabled: true
+        }
+  ];
 
+    $scope.alertMe = function () {
+        setTimeout(function () {
+            $window.alert('You\'ve selected the alert tab!');
+        });
+    };
 
 
 
