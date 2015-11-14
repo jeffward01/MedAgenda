47eb309c7846cccf9068e003246cae69b6bb87df
jeffward01 <jeffwardcoding@gmail.com>
2015-11-14 15:58:22 -0800

    added current appointments

--- MedAgenda/MedAgenda.Client/js/app.js
+++ MedAgenda/MedAgenda.Client/js/app.js
@@ -27,3 +27,48 @@ angular.module('app', ['ui.router', 'ui.bootstrap', 'ngResource','angular-loadin
 angular.module('app').value('apiUrl', 'http://localhost:65406/api/');
 
 
+//Telephone Filter Code
+angular.module('app').filter('tel', function () {
+    return function (tel) {
+        if (!tel) { return ''; }
+
+        var value = tel.toString().trim().replace(/^\+/, '');
+
+        if (value.match(/[^0-9]/)) {
+            return tel;
+        }
+
+        var country, city, number;
+
+        switch (value.length) {
+            case 10: // +1PPP####### -> C (PPP) ###-####
+                country = 1;
+                city = value.slice(0, 3);
+                number = value.slice(3);
+                break;
+
+            case 11: // +CPPP####### -> CCC (PP) ###-####
+                country = value[0];
+                city = value.slice(1, 4);
+                number = value.slice(4);
+                break;
+
+            case 12: // +CCCPP####### -> CCC (PP) ###-####
+                country = value.slice(0, 3);
+                city = value.slice(3, 5);
+                number = value.slice(5);
+                break;
+
+            default:
+                return tel;
+        }
+
+        if (country == 1) {
+            country = "";
+        }
+
+        number = number.slice(0, 3) + '-' + number.slice(3);
+
+        return (country + " (" + city + ") " + number).trim();
+    };
+});
