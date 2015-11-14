0c4f4de6dd9b79f6badabf71219eb026005aeaed
kds-snyder <kds_snyder@yahoo.com>
2015-11-13 16:43:45 -0800

    Fix missing release folder for angular-ui-router

--- MedAgenda/MedAgenda.Client/js/app/administration/admin.ctrl.js
+++ MedAgenda/MedAgenda.Client/js/app/administration/admin.ctrl.js
@@ -1,3 +1,22 @@
-﻿angular.module('app').controller('AdminController', function ($rootScope, $scope) {
+﻿angular.module('app').controller('AdminController', function ($rootScope, $scope, $state) {
     $rootScope.$broadcast('change-page-title', { title: 'Administration' });
+
+    $scope.tabs = [
+        { heading: "Exam Rooms", route: "app.admin.exam", active: false },
+        { heading: "Specialties", route: "app.admin.specialties", active: false }
+    ];
+
+    $scope.go = function (route) {
+        $state.go(route);
+    };
+
+    $scope.active = function (route) {
+        return $state.is(route);
+    };
+
+    $scope.$on("$stateChangeSuccess", function () {
+        $scope.tabs.forEach(function (tab) {
+            tab.active = $scope.active(tab.route);
+        });
+    });
 });
