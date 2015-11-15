ecd58c8aaffa14d2e879d1ef56d09867d4df3a46
kds-snyder <kds_snyder@yahoo.com>
2015-11-14 21:23:14 -0800

    Started to add <add> for exam rooms

--- MedAgenda/MedAgenda.Client/js/app/administration/admin.examrooms.ctrl.js
+++ MedAgenda/MedAgenda.Client/js/app/administration/admin.examrooms.ctrl.js
@@ -4,6 +4,21 @@
 
     $scope.examRooms = ExamRoom.query();
 
+    $scope.examRoom = new ExamRoom();
+
+    // Save new exam room
+    $scope.saveExamRoom = function () {
+        if ($scope.examRoomForm.$invalid) {
+            toastr.warning('Please verify that you have filled in the exam room');
+            return;
+        }
+
+        $scope.examRoom.$save(function () {
+            toastr.success($scope.examRoom.ExamRoomName + ' was added successfully');
+            
+        });
+    };
+
     $scope.deleteExamRoom = function (examRoom) {
         if (confirm('Are you sure you want to delete exam room: ' +
                                                     examRoom.ExamRoomName + '?')) {
@@ -12,6 +27,9 @@
                 $scope.examRooms.splice(index, 1);
                 toastr.success('The exam room ' +
                                 examRoom.ExamRoomName + ' was deleted successfully');
+            },
+            function (error) {               
+                toastr.error(error.data.Message);
             });
         }
     }
