706b52a38c2f5c23bf9cb406d1e16fefdd75216e
Zak <diezel37@gmail.com>
2015-11-16 12:43:43 -0800

    Changed some punctuation
    Took of the S off the end of patient. (Patients to patient)

---
+++ MedAgenda/MedAgenda.Client/js/app/patients/Detail/patient.detail.ctrl.js
@@ -0,0 +1,20 @@
+﻿angular.module('app').controller('PatientsDetailController', function () {
+
+    if ($stateParams.id) {
+        $scope.patient = Patient.get({ id: $stateParams.id });
+    } else {
+        $scope.patient = new Patient();
+    }
+
+    $scope.saveTenant = function () {
+        if ($scope.patient.PatientId) {
+            $scope.patient.$update(function () {
+                $state.go('patient.grid');
+            });
+        } else {
+            $scope.patient.$save(function () {
+                $state.go('patient.grid');
+            });
+        }
+    }
+});
---
+++ MedAgenda/MedAgenda.Client/js/app/patients/Grid/patient.grid.ctrl.js
@@ -0,0 +1,21 @@
+﻿angular.module('app').controller('PatientsGridController', function ($rootScope) {
+    $rootScope.$broadcast('change-page-title', { title: 'Patients' });
+
+    if ($stateParams.id) {
+        $scope.patient = Patient.get({ id: $stateParams.id });
+    } else {
+        $scope.patient = new Patient();
+    }
+
+    $scope.savePatient = function () {
+        if ($scope.patient.PatientId) {
+            $scope.patient.$update(function () {
+                $state.go('patient.grid');
+            });
+        } else {
+            $scope.patient.$save(function () {
+                $state.go('patient.grid');
+            });
+        }
+    }
+});
