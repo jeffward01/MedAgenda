7e4f08eb2c66beb6cead2ff4f049d36f8c12df31
Cameron Wilby <cameron@origincodeacademy.com>
2015-11-19 11:20:51 -0800

    Added loadmask

--- MedAgenda/MedAgenda.Client/js/app/doctors/doctors.grid.ctrl.js
+++ MedAgenda/MedAgenda.Client/js/app/doctors/doctors.grid.ctrl.js
@@ -1,11 +1,21 @@
 ﻿angular.module('app').controller('DoctorsGridController', function($rootScope, Doctor, $scope) {
-    $scope.doctors = Doctor.query();
 
-    $scope.deleteDoctor= function(doctor) {
-        if(confirm('Are you sure you want to delete this doctor?')) {
+
+    $scope.load = function () {
+        $scope.loading = true;
+        $scope.doctors = Doctor.query(function () {
+            $scope.loading = false;
+        });
+    };
+    
+
+    $scope.deleteDoctor = function (doctor) {
+        if (confirm('Are you sure you want to delete this doctor?')) {
             Doctor.delete({ id: doctor.DoctorId }, function (data) {
                 $scope.doctor = Doctor.query();
-            }); 
-        }   
-    }
+            });
+        }
+    };
+
+    $scope.load();
 });
