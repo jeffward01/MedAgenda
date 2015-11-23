b5facb9ac3cc4f1d61679aea23ef34938330a697
ABracken <adriannbracken214@gmail.com>
2015-11-23 07:44:48 -0800

    Menu hamburger fixed

--- MedAgenda/MedAgenda.Client/js/app/app.ctrl.js
+++ MedAgenda/MedAgenda.Client/js/app/app.ctrl.js
@@ -4,4 +4,63 @@
     $rootScope.$on('change-page-title', function (event, args) {
         $scope.pageTitle = args.title;
     });
+
+
+    /** ******  left menu  *********************** **/
+    $(function () {
+        $('#sidebar-menu li ul').slideUp();
+        $('#sidebar-menu li').removeClass('active');
+
+        $('#sidebar-menu li').click(function () {
+            if ($(this).is('.active')) {
+                $(this).removeClass('active');
+                $('ul', this).slideUp();
+                $(this).removeClass('nv');
+                $(this).addClass('vn');
+            } else {
+                $('#sidebar-menu li ul').slideUp();
+                $(this).removeClass('vn');
+                $(this).addClass('nv');
+                $('ul', this).slideDown();
+                $('#sidebar-menu li').removeClass('active');
+                $(this).addClass('active');
+            }
+        });
+
+        $('#menu_toggle').click(function () {
+            if ($('body').hasClass('nav-md')) {
+                $('body').removeClass('nav-md');
+                $('body').addClass('nav-sm');
+                $('.left_col').removeClass('scroll-view');
+                $('.left_col').removeAttr('style');
+                $('.sidebar-footer').hide();
+
+                if ($('#sidebar-menu li').hasClass('active')) {
+                    $('#sidebar-menu li.active').addClass('active-sm');
+                    $('#sidebar-menu li.active').removeClass('active');
+                }
+            } else {
+                $('body').removeClass('nav-sm');
+                $('body').addClass('nav-md');
+                $('.sidebar-footer').show();
+
+                if ($('#sidebar-menu li').hasClass('active-sm')) {
+                    $('#sidebar-menu li.active-sm').addClass('active');
+                    $('#sidebar-menu li.active-sm').removeClass('active-sm');
+                }
+            }
+        });
+    });
+
+    /* Sidebar Menu active class */
+    $(function () {
+        var url = window.location;
+        $('#sidebar-menu a[href="' + url + '"]').parent('li').addClass('current-page');
+        $('#sidebar-menu a').filter(function () {
+            return this.href == url;
+        }).parent('li').addClass('current-page').parent('ul').slideDown().parent().addClass('active');
+    });
+
+    /** ******  /left menu  *********************** **/
+
 });
